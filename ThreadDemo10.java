7edece19d4e6f260068930ead82ce08437a7e621
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2025-09-16 23:01:03 +0530

    Example of naming and running threads created using both Thread class and Runnable interface

---
+++ ThreadDemo10.java
@@ -0,0 +1,33 @@
+
+// Extending Thread 
+class MyThread extends Thread
+{
+    public void run()
+    {
+        System.out.println("Thred From ThreadClass "+ getName());       // Internally ->   this.getName(); 
+    }
+}
+
+// Implementing Runnable Interface 
+class MyThread1 implements Runnable 
+{
+    public void run()
+    {
+        System.out.println("Thread From Runnable Interface : "+ Thread.currentThread().getName());
+    }
+}
+
+public class ThreadDemo10
+{
+    public static void main(String Arg[])
+    {
+        MyThread Mobj = new MyThread();
+        Mobj.setName("First Thread ");
+        Mobj.start();
+
+        MyThread1 Mobj1 = new MyThread1();
+        Thread tobj = new Thread(Mobj1);
+        tobj.setName("Scond Thread ");          // Set Name to the Thread Class 
+        tobj.start();
+    }
+} 
