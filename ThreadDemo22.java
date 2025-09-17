3a61175c92ca2c3ea8ede7e8c58ac397721ad434
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2025-09-17 19:17:51 +0530

    Add yield() example with Thread, Runnable, and main thread execution

---
+++ ThreadDemo22.java
@@ -0,0 +1,40 @@
+// Thread subclass
+class MyThreadYield extends Thread {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Thread class running: " + i);
+            Thread.yield(); // suggest scheduler to give chance to others
+        }
+        System.out.println("Thread class finished.");
+    }
+}
+
+// Runnable implementation
+class MyRunnableYield implements Runnable {
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Runnable running: " + i);
+            Thread.yield(); // suggest scheduler to give chance to others
+        }
+        System.out.println("Runnable finished.");
+    }
+}
+
+public class ThreadDemo22 {
+    public static void main(String[] args) {
+        System.out.println("=== Using Thread subclass ===");
+        MyThreadYield t1 = new MyThreadYield();
+        t1.start();
+
+        System.out.println("=== Using Runnable implementation ===");
+        Thread t2 = new Thread(new MyRunnableYield());
+        t2.start();
+
+        // Main thread also prints
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Main thread running: " + i);
+            Thread.yield();
+        }
+        System.out.println("Main thread finished.");
+    }
+}
