f02eb7ec394396ab1d39b428bf1ebd7179cfd437
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2025-11-19 21:23:39 +0530

    loop in String

---
+++ Strings in Java/0206_program.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class program_0206
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iCnt = 0;
+
+        System.out.println("Enter String : ");
+        String name = sobj.nextLine();
+
+        for(iCnt = 0; iCnt < name.length(); iCnt++)
+        {   
+            System.out.println(name.charAt(iCnt));
+
+            sobj.close();
+        }
+    }
+}
