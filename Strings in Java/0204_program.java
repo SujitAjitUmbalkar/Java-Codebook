d734af4dfd7e900de240da811845ba9cca4f7237
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2025-11-19 21:22:18 +0530

    To Count Length of String we use .length() methos

---
+++ Strings in Java/0204_program.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class program_0204
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter your name : ");
+        String name = sobj.nextLine();
+
+        System.out.println("Your name is : "+name);
+
+        System.out.println("String length is : "+name.length());
+
+        sobj.close();
+    }
+}
