9dbd0310647f7fa2f03b9b420e1c9cfcbd628cfa
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2025-11-19 22:19:54 +0530

    Improve 1 / Count Small case characters

---
+++ Strings in Java/0210_program.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+class MarvellousString
+{
+    public int CountSmall(String str)
+    {
+        char Arr[] = str.toCharArray();
+
+        int iCount = 0, iCnt = 0;
+
+        for(iCnt = 0; iCnt < Arr.length; iCnt++)
+        {
+            if((Arr[iCnt] >= 'a') && (Arr[iCnt] <= 'z'))
+            {
+                iCount++;
+            }
+        }
+
+        return iCount;
+    }
+}
+
+class program_0210
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Please enter string : ");
+        String str = sobj.nextLine();
+
+        MarvellousString mobj = new MarvellousString();
+
+        int iRet = 0;
+
+        iRet = mobj.CountSmall(str);
+
+        System.out.println("Number of small characters are : "+iRet);
+
+        sobj.close();
+    }
+}
