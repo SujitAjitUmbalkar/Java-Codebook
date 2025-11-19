c6cd58b629218e21f09145db536bc73b3f46c29a
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2025-11-19 21:21:16 +0530

    Initialisation of String / nextLine()

---
+++ Strings in Java/0203_program.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class program_0203
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter your name : ");
+        String name = sobj.nextLine();
+
+        System.out.println("Your name is : "+name);
+
+        sobj.close();
+    }
+}
