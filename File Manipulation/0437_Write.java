de0d4efd29fbb8d219fa4e31c7659c18f2374879
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2025-11-22 22:09:27 +0530

    Correct way to write in file / Use Byte Array

---
+++ File Manipulation/0437_Write.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+class program437
+{
+    public static void main(String A[])
+    {
+        try
+        {
+            Scanner sobj = new Scanner(System.in);
+
+            System.out.println("Enter the name of file that you want to create : ");
+            String Fname = sobj.nextLine();
+
+            File fobj = new File(Fname);
+
+            if(fobj.exists())
+            {
+                FileOutputStream foobj = new FileOutputStream(fobj);
+                String str = "Jay Ganesh...";
+
+                byte Arr[] = str.getBytes();
+
+                foobj.write(Arr);
+                
+                foobj.close();
+            }
+            else
+            {
+                System.out.println("File not present in current directory..");
+                sobj.close();
+                return;
+            }
+
+            sobj.close();
+        }
+        catch(IOException iobj)
+        {}
+        catch(Exception eobj)
+        {}
+    }
+}
