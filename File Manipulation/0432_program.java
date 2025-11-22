1c68e5cfca0ec2229b80fde9f397e8f78f2178c4
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2025-11-22 20:23:53 +0530

    Create File / Fobj.createNewFile()

---
+++ File Manipulation/0432_program.java
@@ -0,0 +1,18 @@
+
+import java.io.*;
+
+class program_0432
+{
+    public static void main(String A[])
+    {
+        try
+        {
+            File fobj = new File("Marvellous.txt");
+            fobj.createNewFile();
+        }
+        catch(IOException iobj)
+        {}
+        catch(Exception eobj)
+        {}
+    }
+}
