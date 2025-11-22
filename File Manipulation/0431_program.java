07d021c556fc9b6567d618fd34c66ae7c4415381
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2025-11-22 20:20:53 +0530

    File Operations can Throw Exception / use try,catch blocks / you need to create object of each file you want to operate on

---
+++ File Manipulation/0431_program.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+class program_0431
+{
+    public static void main(String A[]) throws Exception
+    {
+        File fobj = new File("Marvellous.txt");
+
+        fobj.createNewFile();
+    }
+} 
