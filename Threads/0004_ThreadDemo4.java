f78129baa7a0b219961577cddae0ab67536125e2
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2025-11-26 22:03:48 +0530

    Renaming Thread / setName()

---
+++ Threads/0004_ThreadDemo4.java
@@ -0,0 +1,19 @@
+class Demo extends Thread
+{
+    public void run()
+    {
+        System.out.println("Thread is running" + Thread.currentThread().getName()); 
+    }
+}
+
+class ThreadDemo4_0004
+{
+    public static void main(String A[])
+    {
+        System.out.println("Inside main thread");
+        
+        Demo dobj = new Demo();
+        dobj.setName("PPA");
+        dobj.start();
+    }
+}
