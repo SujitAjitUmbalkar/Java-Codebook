8f5021eb1a7c51330faee99918de6c2de75209a5
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2025-11-26 22:46:17 +0530

    setDaemon(true) / isDaemon() / use before start()

---
+++ Threads/0015_ThreadDemo15.java
@@ -0,0 +1,30 @@
+// Extending Thread
+class MyThread7 extends Thread {
+    public void run() { 
+        System.out.println("Daemon (extends Thread)? " + isDaemon()); 
+    }
+}
+
+// Implementing Runnable
+class MyRunnable7 implements Runnable {
+    public void run() { 
+        System.out.println("Daemon (implements Runnable)? " + Thread.currentThread().isDaemon()); 
+    }
+}
+
+ class ThreadDemo15_0015
+{
+    public static void main(String[] args) {
+        // Using Thread class
+        MyThread7 t1 = new MyThread7();
+        t1.setDaemon(true);   // Must set before start()
+        t1.start();
+
+        // Using Runnable
+        MyRunnable7 r = new MyRunnable7();
+        Thread t2 = new Thread(r);
+        t2.setDaemon(false);  // Default is false, but showing explicitly
+        t2.start();
+    }
+}
+
