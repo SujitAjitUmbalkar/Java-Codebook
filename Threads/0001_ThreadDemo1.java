68ff35b6daf22fdbf11c255cdc35e697718eaecd
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2025-11-26 20:12:19 +0530

    Name of Currently Running Thread / Thread.currentThread().getName()

---
+++ Threads/0001_ThreadDemo1.java
@@ -0,0 +1,8 @@
+class ThreadDemo1_0001
+{
+    public static void main(String A[])
+    {
+        System.out.println("Name of current Thread is:"+Thread.currentThread().getName());
+        
+    }
+}
